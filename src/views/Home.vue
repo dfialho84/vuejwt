e2268d7030ce6f509ce48c44f65fb5a0cf6339b0
Diego Fialho Rodrigues <dfialho84@gmail.com>
2021-11-18 11:17:56 -0300

    Chamada a endpoint com autorizacao

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,15 +4,26 @@
     <div>TICKET: {{ ticket }}</div>
     <div>EXP: {{ exp }}</div>    
     <div>NOW: {{ agora }}</div>    
+    <button class="bg-gray-500" @click="buscarPublico()">Buscar Publico</button>
+    <div>{{ publico }}</div>
+    <button class="bg-gray-500" @click="buscarPrivado()">Buscar Privado</button>
+    <div>{{ privado }}</div>
   </div>
 </template>
 
 <script>
 
 import { mapGetters } from 'vuex';
+import axios from 'axios';
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      publico: "NAO DEFINIDO",
+      privado: "NAO DEFINIDO",
+    }
+  },
   computed: {
     agora() {
       return new Date();
@@ -24,6 +35,26 @@ export default {
       'token', 'ticket'
     ])
     
-  }
+  },
+  methods: {
+    buscarPublico() {
+      axios.get('http://localhost:8081/public').then(resp => {
+        console.log(resp);
+        this.publico = resp.data;
+      });
+    },
+    buscarPrivado() {
+      const options = {
+        headers: {
+          Authorization: this.token
+        }
+
+      }
+      axios.get('http://localhost:8081/protected', options).then(resp => {
+        console.log(resp);
+        this.privado = resp.data;
+      });
+    }
+  }  
 }
 </script>
